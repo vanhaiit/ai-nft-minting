f7f994290a656adf1249315c7e709c87221b3c76
hai.le <hai.le@sotatek.com>
2024-03-17 20:11:23 +0700

    add antd

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Button } from "antd";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Button type="primary">Button</Button>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const LOCAL_STORAGE_KEY = {};
+
+export const MAX_LENGTH_USERNAME = 24;
+
+export const MAX_LENGTH_PHONE_NUMBER = 15;
