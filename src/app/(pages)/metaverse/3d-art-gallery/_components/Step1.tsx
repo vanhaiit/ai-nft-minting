4d1c92032fb5474e43542cf824183e860095c53e
hai.le <hai.le@sotatek.com>
2024-03-18 16:56:40 +0700

    fux

---
+++ src/app/(pages)/metaverse/3d-art-gallery/_components/Step1.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useWindowSize } from "@/hooks";
+import { TypeAnimation } from "react-type-animation";
+import { twMerge } from "tailwind-merge";
+import CommonContainer from "@/app/_components/CommonContainer";
+import CommonButton from "@/app/_components/CommonButton";
+import WaveAnimation from "@/app/_components/WaveAnimation";
+
+function Step1() {
+  const { windowWidth, windowHeight } = useWindowSize();
+  return (
+    <div className="w-full">
+      <div
+        className={twMerge("w-full h-full", "flex flex-col justify-between")}
+      >
+        <CommonContainer containerClassName="py-20">
+          <p className="w-3/4 !text-white text-[50px] font-bold text-space_mono uppercase">
+            <TypeAnimation
+              sequence={[
+                "Get ready for the imminent mass adoption of artificial intelligence...",
+              ]}
+              className="!text-white"
+              cursor={false}
+            />
+          </p>
+        </CommonContainer>
+        <CommonContainer containerClassName="border-t">
+          <div className="grid grid-cols-2">
+            <p className="text-xl pt-12 pr-16 pb-4 border-r !text-white">
+              <TypeAnimation
+                sequence={[
+                  `AI technology will serve as a catalyst for innovation by converging
+                 various technological fields such as blockchain, energy storage,
+                 robotics, and multiomic sequencing....`,
+                ]}
+                className="!text-white"
+                cursor={false}
+              />
+            </p>
+            <p className="text-xl pt-12 ml-16 pb-4 !text-white">
+              <TypeAnimation
+                sequence={[
+                  `Maximize the AI technology to expand the value of your Non-fungible
+                 token and add more value... Alpha Quark allows you to mint NFTs
+                 using the latest version of image generative AI model.`,
+                ]}
+                className="!text-white"
+                cursor={false}
+              />
+            </p>
+          </div>
+          <CommonButton className="w-fit mt-4 mx-auto">Generate</CommonButton>
+        </CommonContainer>
+        <WaveAnimation width={windowWidth} height={windowHeight / 2.5} />
+      </div>
+    </div>
+  );
+}
+
+export default Step1;
---
+++ src/app/(pages)/metaverse/3d-art-gallery/_components/Step4.tsx
@@ -0,0 +1,94 @@
+import React, { ComponentPropsWithoutRef } from "react";
+import { twMerge, twJoin } from "tailwind-merge";
+import { ArrowIcon } from "@/app/_components/icon";
+import { TypeAnimation } from "react-type-animation";
+
+import CommonButton from "@/app/_components/CommonButton";
+import CommonContainer from "@/app/_components/CommonContainer";
+
+const Section2 = () => {
+  const sequence = [
+    "Enter keywords as you want...",
+    "We will generate and visualize whatever you imagine...",
+  ];
+
+  return (
+    <div className={twMerge("w-full h-full", "flex flex-col ")}>
+      <CommonContainer
+        containerClassName="w-full pt-20 pb-6"
+        className="justify-center gap-y-8"
+      >
+        {sequence.map((item, index) => (
+          <TypeAnimation
+            key={index}
+            sequence={[item]}
+            wrapper="p"
+            cursor={false}
+            className="w-3/4 text-2xl font-bold text-space_mono uppercase"
+          />
+        ))}
+        <div className={twJoin("w-full h-full", "flex items-center gap-x-6")}>
+          <TopSection2Item
+            title="A beautiful forest and futuristic city where there are a lot of artistic buildings."
+            labelButton="Generate"
+          />
+          <TopSection2Item
+            title="Upload your image that you want to make the AI model refer to"
+            labelButton="Select"
+          />
+        </div>
+        <CommonButton className="w-fit mx-auto">Upload</CommonButton>
+      </CommonContainer>
+      <CommonContainer containerClassName="border-t">
+        <div className="grid grid-cols-2">
+          <p className="text-xl pt-12 pr-16 pb-4 border-r">
+            AI technology will serve as a catalyst for innovation by converging
+            various technological fields such as blockchain, energy storage,
+            robotics, and multiomic sequencing....
+          </p>
+          <p className="text-xl pt-12 ml-16 pb-4">
+            Maximize the AI technology to expand the value of your Non-fungible
+            token and add more value... Alpha Quark allows you to mint NFTs
+            using the latest version of image generative AI model.
+          </p>
+        </div>
+      </CommonContainer>
+    </div>
+  );
+};
+
+export default Section2;
+
+const TopSection2Item: React.FC<TopSection2ItemProps> = ({
+  title,
+  children,
+  className,
+  labelButton,
+  onClickButton,
+  ...otherProps
+}) => {
+  return (
+    <div
+      className={twMerge(
+        "p-4",
+        "border border-neutral4",
+        "w-full h-full min-h-[148px]",
+        "flex flex-col justify-between",
+        className
+      )}
+      {...otherProps}
+    >
+      <p className="font-medium">{title}</p>
+      <button className="flex items-center gap-x-1 ml-auto text-sm text-neutral4">
+        {labelButton}
+        <ArrowIcon />
+      </button>
+    </div>
+  );
+};
+
+interface TopSection2ItemProps extends ComponentPropsWithoutRef<"div"> {
+  title: string;
+  labelButton: string;
+  onClickButton?: () => void;
+}
